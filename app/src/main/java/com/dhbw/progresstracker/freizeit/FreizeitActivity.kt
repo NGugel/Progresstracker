fb34dc354c4bd9ecacc6a36961337b534264315c
Niclas <freestyle.nici@gmx.de>
2023-11-15 20:33:30 +0100

    Freizeit Untermenü umstrukturiert, sodass ein "Add New Button"-Button existiert, mit dem neue Buttons erstellt werden können

--- app/src/main/java/com/dhbw/progresstracker/freizeit/FreizeitActivity.kt
+++ app/src/main/java/com/dhbw/progresstracker/freizeit/FreizeitActivity.kt
@@ -1,10 +1,13 @@
 package com.dhbw.progresstracker.freizeit
 
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
+import androidx.core.view.marginTop
 import com.dhbw.progresstracker.MainActivity
 import com.dhbw.progresstracker.R
 
@@ -15,20 +18,35 @@ class FreizeitActivity : ComponentActivity() {
 
         val zurueckbutton = findViewById<Button>(R.id.zurueckbutton)
 
-        val verwaltungsbutton = findViewById<Button>(R.id.verwaltungsbutton)
-        val anwendungsbutton = findViewById<Button>(R.id.anwendungsbutton)
+        val addButton = findViewById<Button>(R.id.addButton)
+
+        val buttonLayout = findViewById<LinearLayout>(R.id.buttonLayout2)
 
         zurueckbutton.setOnClickListener {
             Log.d("FreizeitActivity", "Hello World von Zurueckbutton!")
             startActivity(Intent(this, MainActivity::class.java))
         }
 
-        verwaltungsbutton.setOnClickListener {
-            Log.d("FreizeitActivity", "Hello World von Verwaltungsbutton!")
+        addButton.setOnClickListener {
+            Log.d("FreizeitActivity", "Hello World von Addbutton!")
+            createButton(buttonLayout, addButton.width, addButton.height, addButton.marginTop)
         }
+    }
 
-        anwendungsbutton.setOnClickListener {
-            Log.d("FreizeitActivity", "Hello World von Anwendungsbutton!")
+    private fun createButton(buttonLayout: LinearLayout, width: Int, height: Int, marginTop: Int) {
+        val newButton = Button(this)
+        newButton.text = "Neuer Button"
+        newButton.typeface = Typeface.DEFAULT_BOLD
+        newButton.layoutParams = LinearLayout.LayoutParams(
+            width,
+            height
+        )
+        val layoutParams = newButton.layoutParams as LinearLayout.LayoutParams
+        layoutParams.setMargins(0, marginTop, 0, 0) // Abstand zwischen den Buttons festlegen
+        newButton.setBackgroundResource(R.drawable.test_herbstbild)
+        newButton.setOnClickListener {
+            // Aktion für den neuen Button hier definieren
         }
+        buttonLayout.addView(newButton, buttonLayout.childCount - 1)
     }
 }
