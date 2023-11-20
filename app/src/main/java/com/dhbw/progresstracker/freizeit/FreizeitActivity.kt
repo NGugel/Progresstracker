149118a4629f1fb0f208b4a0789b51c44b931304
Niclas <freestyle.nici@gmx.de>
2023-11-20 23:03:24 +0100

    lokale Speichern von Buttons funktioniert nun und ein onTouchListener wurde benutzt anstelle eines onClickListeners

--- app/src/main/java/com/dhbw/progresstracker/freizeit/FreizeitActivity.kt
+++ app/src/main/java/com/dhbw/progresstracker/freizeit/FreizeitActivity.kt
@@ -1,12 +1,20 @@
 package com.dhbw.progresstracker.freizeit
 
+import OnSwipeTouchListener
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.ViewTreeObserver
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.marginTop
 import com.dhbw.progresstracker.MainActivity
 import com.dhbw.progresstracker.R
@@ -22,6 +30,8 @@ class FreizeitActivity : ComponentActivity() {
 
         val buttonLayout = findViewById<LinearLayout>(R.id.buttonLayout2)
 
+        loadButtons(buttonLayout, addButton)
+
         zurueckbutton.setOnClickListener {
             Log.d("FreizeitActivity", "Hello World von Zurueckbutton!")
             startActivity(Intent(this, MainActivity::class.java))
@@ -34,8 +44,54 @@ class FreizeitActivity : ComponentActivity() {
     }
 
     private fun createButton(buttonLayout: LinearLayout, width: Int, height: Int, marginTop: Int) {
+        showInputDialog { buttonName ->
+            val newButton = Button(this)
+            newButton.text = buttonName
+            newButton.typeface = Typeface.DEFAULT_BOLD
+            newButton.layoutParams = LinearLayout.LayoutParams(
+                width,
+                height
+            )
+            val layoutParams = newButton.layoutParams as LinearLayout.LayoutParams
+            layoutParams.setMargins(0, marginTop, 0, 0) // Abstand zwischen den Buttons festlegen
+            newButton.setBackgroundResource(R.drawable.test_herbstbild)
+
+            // Swipe-Geste erkennen und Mülleimer-Icon anzeigen
+            val gestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
+                override fun onFling(
+                    e1: MotionEvent,
+                    e2: MotionEvent,
+                    velocityX: Float,
+                    velocityY: Float
+                ): Boolean {
+                    if (Math.abs(velocityX) > Math.abs(velocityY)) {
+                        if (velocityX < 0) {
+                            // Swipe nach links erkannt - Mülleimer-Icon anzeigen
+                            newButton.setCompoundDrawablesWithIntrinsicBounds(
+                                R.drawable.delete_icon,
+                                0,
+                                0,
+                                0
+                            )
+                            return true
+                        }
+                    }
+                    return false
+                }
+            })
+
+            newButton.setOnClickListener {
+                // Aktion für den neuen Button hier definieren
+                Log.d("FreizeitActivity", buttonName.toString() )
+            }
+            buttonLayout.addView(newButton, buttonLayout.childCount - 1)
+            saveButton(buttonName)
+        }
+    }
+
+    private fun createButton(buttonLayout: LinearLayout, width: Int, height: Int, marginTop: Int, buttonName: String) {
         val newButton = Button(this)
-        newButton.text = "Neuer Button"
+        newButton.text = buttonName
         newButton.typeface = Typeface.DEFAULT_BOLD
         newButton.layoutParams = LinearLayout.LayoutParams(
             width,
@@ -44,9 +100,132 @@ class FreizeitActivity : ComponentActivity() {
         val layoutParams = newButton.layoutParams as LinearLayout.LayoutParams
         layoutParams.setMargins(0, marginTop, 0, 0) // Abstand zwischen den Buttons festlegen
         newButton.setBackgroundResource(R.drawable.test_herbstbild)
+
+        // OnTouchListener, um einen Swipe oder Touch zu erkennen
+        newButton.setOnTouchListener(object : OnSwipeTouchListener(this) {
+            override fun onSwipeLeft() {
+                // Mülleimer-Icon anzeigen oder Aktion ausführen
+                Log.d("FreizeitActivity", "Swiped left on button $buttonName")
+
+                /*
+                // Swipe nach links erkannt - Mülleimer-Icon anzeigen
+                val deleteIcon = ContextCompat.getDrawable(this@FreizeitActivity, R.drawable.delete_icon)
+                deleteIcon?.setBounds(newButton.width - newButton.width / 4 - deleteIcon.intrinsicWidth, 0, newButton.width / 4, newButton.height * 3 / 4)
+                newButton.setCompoundDrawables(deleteIcon, null, null, null)
+
+                 */
+
+                /*
+                // Button rechts anzeigen
+                val layoutParams = newButton.layoutParams as RelativeLayout.LayoutParams
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_END)
+                newButton.layoutParams = layoutParams
+
+                 */
+            }
+            override fun onSwipeRight() {
+                // Mülleimer-Icon anzeigen oder Aktion ausführen
+                Log.d("FreizeitActivity", "Swiped right on button $buttonName")
+            }
+            override fun onTouch() {
+                // Mülleimer-Icon anzeigen oder Aktion ausführen
+                Log.d("FreizeitActivity", "Touch/Click on button $buttonName")
+            }
+        })
+
+        /*
+        // Swipe-Geste erkennen und Mülleimer-Icon anzeigen
+        val gestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onFling(
+                e1: MotionEvent,
+                e2: MotionEvent,
+                velocityX: Float,
+                velocityY: Float
+            ): Boolean {
+                if (Math.abs(velocityX) > Math.abs(velocityY)) {
+                    if (velocityX < 0) {
+                        // Swipe nach links erkannt - Mülleimer-Icon anzeigen
+                        newButton.setCompoundDrawablesWithIntrinsicBounds(
+                            R.drawable.delete_icon,
+                            0,
+                            0,
+                            0
+                        )
+                        return true
+                    }
+                }
+                return false
+            }
+        })
+
+
+         */
+
+        /*
         newButton.setOnClickListener {
             // Aktion für den neuen Button hier definieren
+            Log.d("FreizeitActivity", buttonName.toString() )
         }
+
+         */
         buttonLayout.addView(newButton, buttonLayout.childCount - 1)
     }
+
+    private fun showInputDialog(callback: (String) -> Unit) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Namen der neuen Aktivität eingeben!")
+
+        val input = EditText(this)
+        builder.setView(input)
+
+        builder.setPositiveButton("OK") { _, _ ->
+            val buttonName = input.text.toString()
+            callback(buttonName)
+        }
+
+        builder.setNegativeButton("Abbrechen") { dialog, _ ->
+            dialog.cancel()
+        }
+
+        builder.show()
+    }
+
+    private fun saveButton(buttonName: String) {
+        val sharedPreferences = getSharedPreferences("ButtonPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(buttonName, buttonName)
+        editor.apply()
+    }
+
+    private fun loadButtons(buttonLayout: LinearLayout, addButton: Button) {
+        val sharedPreferences = getSharedPreferences("ButtonPrefs", Context.MODE_PRIVATE)
+        val buttonNames = sharedPreferences.all
+
+        // Warte bis der addButton auf dem Bildschirm angezeigt wird
+        addButton.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                // Entferne den Listener, um ihn nicht mehrmals aufzurufen
+                addButton.viewTreeObserver.removeOnGlobalLayoutListener(this)
+
+                // Rufe die Funktion zur Erstellung der Buttons auf
+                buttonNames?.forEach { buttonName ->
+                    createButton(buttonLayout, addButton.width, addButton.height, addButton.marginTop, buttonName.value.toString())
+                }
+            }
+        })
+
+        buttonNames?.forEach { buttonName ->
+            createButton(buttonLayout, addButton.width, addButton.height, addButton.marginTop, buttonName.value.toString())
+        }
+    }
+
+    private fun getButtonNamesFromLayout(buttonLayout: LinearLayout): List<String> {
+        val buttonNames = mutableListOf<String>()
+        for (i in 0 until buttonLayout.childCount - 1) {
+            val button = buttonLayout.getChildAt(i) as Button
+            buttonNames.add(button.text.toString())
+        }
+        return buttonNames
+    }
+
 }
---
+++ app/src/main/java/com/dhbw/progresstracker/util/OnSwipeTouchListener.kt
@@ -0,0 +1,53 @@
+import android.content.Context
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+
+open class OnSwipeTouchListener(context: Context) : View.OnTouchListener {
+
+    private val gestureDetector: GestureDetector
+
+    init {
+        gestureDetector = GestureDetector(context, GestureListener())
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        return gestureDetector.onTouchEvent(event)
+    }
+
+    private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+
+        private val SWIPE_THRESHOLD = 10
+        private val SWIPE_VELOCITY_THRESHOLD = 10
+
+        override fun onDown(e: MotionEvent): Boolean {
+            onTouch()
+            return true
+        }
+
+        override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+            val distanceX = e2.x - e1.x
+            val distanceY = e2.y - e1.y
+
+            if (Math.abs(distanceX) > Math.abs(distanceY) &&
+                Math.abs(distanceX) > SWIPE_THRESHOLD &&
+                Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                if (distanceX > 0) {
+                    onSwipeRight()
+                } else if(distanceX < 0) {
+                    onSwipeLeft()
+                }
+                return true
+            }
+
+            return false
+        }
+    }
+
+    open fun onTouch() {}
+
+    open fun onSwipeLeft() {}
+
+    open fun onSwipeRight() {}
+}
