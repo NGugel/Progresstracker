310edba43528728c3f421e6890b22c58b8eeb334
Patrick Furtwängler <patrick.furtwaengler@cte.ch>
2023-12-12 16:19:10 +0100

    Buttons ohne Caps im Kategorie Screen, Eingebracht, dass man Kategorien löschen kann

--- app/src/main/java/com/dhbw/progresstracker/lernen/Verwaltung/Kategorie/KategorieVerwaltungActivity.kt
+++ app/src/main/java/com/dhbw/progresstracker/lernen/Verwaltung/Kategorie/KategorieVerwaltungActivity.kt
@@ -3,17 +3,21 @@ package com.dhbw.progresstracker.lernen.Verwaltung.Kategorie
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.dhbw.progresstracker.R
 import com.dhbw.progresstracker.databinding.ActivityKategorieverwaltungBinding
 import com.dhbw.progresstracker.lernen.Verwaltung.VerwaltungsModusActivity
+import com.dhbw.progresstracker.repository.ViewModel
 import com.dhbw.progresstracker.repository.database.Kategorie
 
 
 class KategorieVerwaltungActivity : AppCompatActivity() {
 
     var binding: ActivityKategorieverwaltungBinding? = null
+    private lateinit var viewModel: ViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,11 +36,23 @@ class KategorieVerwaltungActivity : AppCompatActivity() {
         // Jetzt kannst du die ausgewählte Kategorie in dieser Aktivität verwenden
         // ...
 
+        viewModel = ViewModelProvider(this).get(ViewModel::class.java)
+
         val btnAddFrage = findViewById<ImageButton>(R.id.btnAddFrage)
         btnAddFrage.setOnClickListener {
             Log.d("VerwaltenActivity", "Hello World von btnAddFrage!")
             val frageDialog = FrageDialogInput()
             frageDialog.show(supportFragmentManager, "Neue Frage")
+        }
+
+        val btnDeleteKategorie = findViewById<Button>(R.id.btn_DeleteKategorie)
+        btnDeleteKategorie.setOnClickListener {
+            if (empfangeneKategorie != null) {
+                viewModel.delete(empfangeneKategorie)
+
+                startActivity(Intent(this, VerwaltungsModusActivity::class.java))
+            }
+
         }
 
         val zurueckbutton = findViewById<ImageButton>(R.id.zurueckbutton)
