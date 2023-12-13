0cf1ae47422e6dcc6aa4faf2f7dde8158512bed0
Patrick Furtwängler <patrick-f@live.de>
2023-12-13 11:38:19 +0100

    OnFling Fix

--- app/src/main/java/com/dhbw/progresstracker/util/OnSwipeTouchListener.kt
+++ app/src/main/java/com/dhbw/progresstracker/util/OnSwipeTouchListener.kt
@@ -25,24 +25,26 @@ open class OnSwipeTouchListener(context: Context) : View.OnTouchListener {
             return true
         }
 
-        /*override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-            val distanceX = e2.x - e1.x
-            val distanceY = e2.y - e1.y
+        override fun onFling(e1: MotionEvent?, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+            e1?.let {
+                val distanceX = e2.x - e1.x
+                val distanceY = e2.y - e1.y
 
-            if (Math.abs(distanceX) > Math.abs(distanceY) &&
-                Math.abs(distanceX) > SWIPE_THRESHOLD &&
-                Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                if (distanceX > 0) {
-                    onSwipeRight()
-                } else if(distanceX < 0) {
-                    onSwipeLeft()
+                if (Math.abs(distanceX) > Math.abs(distanceY) &&
+                    Math.abs(distanceX) > SWIPE_THRESHOLD &&
+                    Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (distanceX > 0) {
+                        onSwipeRight()
+                    } else if (distanceX < 0) {
+                        onSwipeLeft()
+                    }
+                    return true
                 }
-                return true
             }
 
             return false
         }
-*/
+
         override fun onSingleTapUp(e: MotionEvent): Boolean {
             onTouch()
             return super.onSingleTapUp(e)
